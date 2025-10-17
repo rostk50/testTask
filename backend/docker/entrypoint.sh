c39e67411dbcaccaf93a474948127c30d966ef41
rostik50 <rostik5079@gmail.com>
2025-10-17 19:02:10 +0300

    fix entrypoint and docker

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -11,8 +11,11 @@ mkdir -p \
   /var/www/html/storage/framework/views \
   /var/www/html/bootstrap/cache
 
+touch /var/www/html/storage/logs/laravel.log || true
+
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R ug+rwX           /var/www/html/storage /var/www/html/bootstrap/cache
+chmod 664 /var/www/html/storage/logs/laravel.log || true
 
 rm -f /var/www/html/bootstrap/cache/*.php || true
 
